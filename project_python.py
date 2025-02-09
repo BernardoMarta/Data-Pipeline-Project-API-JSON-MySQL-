a5fbf7576f1b04e91d20c50a383b2f7b1bd1e665
BernardoMarta <71236109+BernardoMarta@users.noreply.github.com>
2025-02-09 23:32:48 +0000

    Create project_python.py

---
+++ project_python.py
@@ -0,0 +1,14 @@
+# Projeto Python- Andressa Lopes, Bernardo Marta, Daniela Esmeraldino.
+ 
+import requests
+import json
+from mysql.connector import connect, Error
+import beaupy
+from functions import *
+
+importDataCategories()
+exportJsonProducts()
+importDataProducts()
+exportJsonReviews()
+importDataReviews()
+menu()
